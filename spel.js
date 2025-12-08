12300c9cc81f57c59f15476e17f6a74976486693
Rasmus carlstedt <cara24fq@student.ju.sem>
2025-12-08 19:38:59 +0100

    player gravity + jump added

--- spel.js
+++ spel.js
@@ -1,7 +1,29 @@
+let playerY = 560; // starting position (vertical)
+let playerVy = 30; // vertical speed
+let gravity = 0.4;
+let jumpForce = -10; // jumping force (more negative = larger jump)
+
 function setup() {
   createCanvas(400, 600);
 }
+
 function draw() {
   background(135, 200, 230);
-  rect(180, 300, 40, 40);
+
+  playerVy += gravity; //gravity applied
+  playerY += playerVy;
+
+  rect(180, playerY, 44, 40); //player
+
+  if (playerY > 560) {
+    playerY = 560; // stop at bottom
+    playerVy = 0; // stop from falling
+  }
+}
+
+function keyPressed() {
+  if (key === "w") {
+    // if "W" pressed jump
+    playerVy = jumpForce;
+  }
 }
