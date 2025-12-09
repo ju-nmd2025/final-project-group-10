9c8f0c99b7d0625fb2415488a441892f84f73657
superemil26 <meem25wi@student.ju.se>
2025-12-09 09:45:38 +0100

    added autobounce and movement left and right

--- spel.js
+++ spel.js
@@ -1,29 +1,35 @@
-let playerY = 560; // starting position (vertical)
-let playerVy = 30; // vertical speed
-let gravity = 0.4;
-let jumpForce = -10; // jumping force (more negative = larger jump)
+let playerY = 560; // vertical position 
+let playerX = 180; // Horizontal position 
+let playerVy = 30; // vertical speed 
+let gravity = 0.4; 
+let jumpForce = -10; // jumping force 
+let moveSpeed = 6; // How fast vertical movement is 
+ 
+function setup() { 
+createCanvas(400, 600); 
+} 
+ 
+function draw() { 
+background(135, 200, 230); 
+ 
+playerVy += gravity; //gravity applied 
+playerY += playerVy; 
+ 
+if (keyIsDown(65)) { 
+playerX -= moveSpeed; // move left 
+} 
+if (keyIsDown(68)) { 
+playerX += moveSpeed; // move right 
+} 
+ 
+rect(playerX, playerY, 44, 40); //player 
+ 
+if (playerY > 560) { 
+playerY = 560; // player start on floor 
+playerVy = jumpForce; // bounce up 
+} 
+} 
 
-function setup() {
-  createCanvas(400, 600);
-}
 
-function draw() {
-  background(135, 200, 230);
+  
 
-  playerVy += gravity; //gravity applied
-  playerY += playerVy;
-
-  rect(180, playerY, 44, 40); //player
-
-  if (playerY > 560) {
-    playerY = 560; // stop at bottom
-    playerVy = 0; // stop from falling
-  }
-}
-
-function keyPressed() {
-  if (key === "w") {
-    // if "W" pressed jump
-    playerVy = jumpForce;
-  }
-}
