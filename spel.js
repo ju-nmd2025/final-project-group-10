b19af8a6e0ece5f559fc9d4eaa79f2c9aa542eba
Rasmus carlstedt <cara24fq@student.ju.sem>
2025-12-10 11:27:50 +0100

    added scroll function and infinite platforms

--- spel.js
+++ spel.js
@@ -4,20 +4,21 @@ class Platform {
     this.y = y; //vertical position
     this.w = 60; // width
     this.h = 10; // height
-    this.type = type;  //"normal" or "breaking"
+    this.type = type; //"normal" or "breaking"
     this.broken = false; // starts as not broken becomes true once bounced
   }
 
   draw() {
-    if (this.type === "breaking" && this.broken) { //does not regenerete platform if broken
+    if (this.type === "breaking" && this.broken) {
+      //does not regenerete platform if broken
       return; //stops the function
     }
-    if (this.type === "breaking") { 
-      fill (200, 0, 0); // breaking platform red
+    if (this.type === "breaking") {
+      fill(200, 0, 0); // breaking platform red
     } else {
-      fill (0, 200, 0); //normal platform green
+      fill(0, 200, 0); //normal platform green
     }
-    
+
     rect(this.x, this.y, this.w, this.h); //player
   }
 }
@@ -33,15 +34,15 @@ let platforms = []; //array
 function setup() {
   createCanvas(400, 600);
 
-  //Generating platforms 
-  for (let i = 0; i < 5; i++) { 
-let x = random(0, 340); // random horizontal position
-let y = 170 + i * 100; // spaced vertically 100 space between every platform
-let type = random(1) < 0.3 ? "breaking" : "normal"; //randomly choose type breaking or normal
-platforms.push(new Platform(x, y, type)); //push into array
+  //Generating platforms
+  for (let i = 0; i < 5; i++) {
+    let x = random(0, 340); // random horizontal position
+    let y = 170 + i * 100; // spaced vertically 100 space between every platform
+    let type = random(1) < 0.3 ? "breaking" : "normal"; //randomly choose type breaking or normal
+    platforms.push(new Platform(x, y, type)); //push into array
+  }
 }
-}
- 
+
 function draw() {
   background(135, 200, 230);
 
@@ -57,8 +58,10 @@ function draw() {
   for (let p of platforms) {
     p.draw();
   }
-  for (let p of platforms) { //check all platforms in array
-    if (p.type === "breaking" && p.broken){//if platform breaking and broken check new
+  for (let p of platforms) {
+    //check all platforms in array
+    if (p.type === "breaking" && p.broken) {
+      //if platform breaking and broken check new
       continue;
     }
     if (playerVy > 0) {
@@ -71,10 +74,32 @@ function draw() {
       if (touchingX && touchingY) {
         //if both are true, bounce
         playerVy = jumpForce;
-      if(p.type === "breaking"){ //breaking platforms should disapear
-        p.broken = true; //marks the platform as true so it doesnt get redrawn
+        if (p.type === "breaking") {
+          //breaking platforms should disapear
+          p.broken = true; //marks the platform as true so it doesnt get redrawn
+        }
       }
-      }
+    }
+  }
+
+  if (playerY < 250) {
+    let scroll = 4; // how fast everything moves
+    playerY += scroll; //push player down scroll amount
+    for (let p of platforms) p.y += scroll; //loops through each platform and adds scroll to it
+  }
+
+  for (let i = 0; i < platforms.length; i++) {
+    let p = platforms[i];
+    if (p.y > height) {
+      // if platforms y is below screen
+      platforms.splice(i, 1); // Remove one platform from array
+      let highestY = Infinity; //we make the the highestY infite to find the smallest platform value
+      for (let p of platforms) if (p.y < highestY) highestY = p.y; //loop through platforms and updates highest Y value
+      let newX = random(0, 340); //create a new X value to platform
+      let newY = highestY - 100; // create a new Y value to platform 100 from highest one
+      let type = random(1) < 0.3 ? "breaking" : "normal"; //make randomly breaking/normal
+      platforms.push(new Platform(newX, newY, type)); //push new platform into array
+      i--; //go back one step in array so it doesent skip a platform
     }
   }
 
